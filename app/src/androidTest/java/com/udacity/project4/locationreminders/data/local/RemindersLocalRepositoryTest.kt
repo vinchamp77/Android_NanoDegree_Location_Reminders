fbf9f3d93b129f32e273e27fa7f79a1f77fcbfe9
vinchamp77 <vinchamp77@gmail.com>
2021-11-08 11:33:17 +1100

    Add database test

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -7,12 +7,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.instanceOf
-import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -25,6 +23,60 @@ import org.junit.runner.RunWith
 @MediumTest
 class RemindersLocalRepositoryTest {
 
-//    TODO: Add testing implementation to the RemindersLocalRepository.kt
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var database: RemindersDatabase
+    private lateinit var repository: RemindersLocalRepository
+
+    @Before
+    fun setup() {
+        // Using an in-memory database so that the information stored here disappears when the
+        // process is killed.
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        ).build()
+
+        repository = RemindersLocalRepository(database.reminderDao())
+    }
+
+    @After
+    fun cleanUp() = database.close()
+
+    // runBlocking is used here because of https://github.com/Kotlin/kotlinx.coroutines/issues/1204
+    // TODO: Replace with runBlockingTest once issue is resolved
+    @Test
+    fun testInsertRetrieveData() = runBlocking {
+
+        val data = ReminderDTO(
+            "title abc",
+            "description abc",
+            "location abc",
+            77.00,
+            77.00)
+
+        repository.saveReminder(data)
+
+        val result = repository.getReminder(data.id)
+
+        result as Result.Success
+        MatcherAssert.assertThat(result.data != null, CoreMatchers.`is`(true))
+
+        val loadedData = result.data
+        MatcherAssert.assertThat(loadedData.id, CoreMatchers.`is`(data.id))
+        MatcherAssert.assertThat(loadedData.title, CoreMatchers.`is`(data.title))
+        MatcherAssert.assertThat(loadedData.description, CoreMatchers.`is`(data.description))
+        MatcherAssert.assertThat(loadedData.location, CoreMatchers.`is`(data.location))
+        MatcherAssert.assertThat(loadedData.latitude, CoreMatchers.`is`(data.latitude))
+        MatcherAssert.assertThat(loadedData.longitude, CoreMatchers.`is`(data.longitude))
+    }
+
+    @Test
+    fun testDataNotFound_returnError() = runBlocking {
+        val result = repository.getReminder("7777")
+        val error =  (result is Result.Error)
+        MatcherAssert.assertThat(error, CoreMatchers.`is`(true))
+    }
 }
